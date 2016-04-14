24cddb8c50f5352dac9fcebbac25f93bfcce9227
seanxkao <sean1201@gmail.com>
2016-04-14 20:37:11 +0800

    Create sjf.cpp

--- project-1/main.cpp
+++ project-1/main.cpp
@@ -42,6 +42,7 @@ int main(int argc, char * argv[]) {
       rrSchedule(N, joblist);
       break;
     case SJF:
+	sjfSchedule(N, joblist);
       break;
     case PSJF:
       break;
---
+++ project-1/sjf.cpp
@@ -0,0 +1,27 @@
+#include "header.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <algorithm>
+#include <functional>
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sched.h>
+#include <queue>
+
+using namespace std;
+
+
+
+//SJF by Sean
+
+void sjfSchedule(int n, vector<Job> job){
+	//shortest job first:
+	//every time a job ends, execute the next shortest job.
+
+	
+}
